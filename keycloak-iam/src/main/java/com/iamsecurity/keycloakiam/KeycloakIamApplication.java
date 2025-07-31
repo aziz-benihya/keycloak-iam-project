15880d6059b7e3ddb08dd09a32cf30836116ece2
aziz-benihya <abdelazizaitbenihya@gmail.com>
2025-07-31 14:21:57 +0100

    Initial commit - ajout structure MVC

---
+++ keycloak-iam/src/main/java/com/iamsecurity/keycloakiam/KeycloakIamApplication.java
@@ -0,0 +1,13 @@
+package com.iamsecurity.keycloakiam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakIamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakIamApplication.class, args);
+	}
+
+}
---
+++ keycloak-iam/src/test/java/com/iamsecurity/keycloakiam/KeycloakIamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamsecurity.keycloakiam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakIamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
